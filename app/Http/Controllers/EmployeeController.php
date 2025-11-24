3f19a62dca236405423e0851edca9975d4f74adc
Yisus550 <jesus.alfonso2402@gmail.com>
2025-11-23 18:30:56 -0700

    feat(employee): Add login logic

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,9 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EmployeeController extends Controller
 {
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        /* $remember = request()->has('remember'); */
+
+        if (Auth::guard('employee')->attempt($credentials/* , $remember */)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/categories');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
     /**
      * Display a listing of the resource.
      */
