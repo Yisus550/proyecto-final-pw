bada2a32e5364c940dc25e4fa14bbf280d209c17
Yisus550 <jesus.alfonso2402@gmail.com>
2025-11-23 18:30:13 -0700

    feat(routes): Add employee custom guard and protecte routes

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,26 @@ use App\Http\Controllers\OrderDetailsController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/login', 'login')->name('login');
+Route::post('/login', [EmployeeController::class, 'login'])->name('login');
+Route::get('/logout', [EmployeeController::class, 'logout'])->name('logout');
+
+Route::middleware('auth:employee')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::resource('categories', CategoryController::class)->parameters(['categories' => 'id']);
+
+    Route::resource('products', ProductController::class)->parameters(['parameters' => 'id']);
+
+    Route::resource('inventories', InventoryController::class)->parameters(['inventories' => 'id']);
+
+    Route::resource('employees', EmployeeController::class)->parameters(['employees' => 'id']);
+
+    Route::resource('customers', CustomerController::class)->parameters(['customers' => 'id']);
+
+    Route::resource('orders', OrderController::class)->parameters(['orders' => 'id']);
+
+    Route::resource('order-details', OrderDetailsController::class)->parameters(['order-details' => 'id']);
 });
-
-Route::resource('categories', CategoryController::class)->parameters(['categories' => 'id']);
-
-Route::resource('products', ProductController::class)->parameters(['parameters' => 'id']);
-
-Route::resource('inventories', InventoryController::class)->parameters(['inventories' => 'id']);
-
-Route::resource('employees', EmployeeController::class)->parameters(['employees' => 'id']);
-
-Route::resource('customers', CustomerController::class)->parameters(['customers' => 'id']);
-
-Route::resource('orders', OrderController::class)->parameters(['orders' => 'id']);
-
-Route::resource('order-details', OrderDetailsController::class)->parameters(['order-details' => 'id']);
