93104fa08eb7f7c2cbcdb686c6157262bb2e20cc
Yisus550 <jesus.alfonso2402@gmail.com>
2025-11-23 18:26:55 -0700

    feat(views): Add login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>POS Login</title>
+</head>
+
+<body>
+    <div>
+        <h2>Iniciar Sesión en POS</h1>
+            @error('email')
+                <div style="color: red;">
+                    {{ session('error') }}
+                </div>
+            @enderror
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div>
+                    <label for="email">Correo electronico:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div>
+                    <label for="password">Contraseña:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <!-- <div>
+                    <label for="remember">
+                        <input type="checkbox" id="remember" name="remember">
+                        Recuérdame
+                    </label>
+                </div> -->
+                <div>
+                    <button type="submit">Iniciar Sesión</button>
+                </div>
+            </form>
+    </div>
+</body>
+
+</html>
